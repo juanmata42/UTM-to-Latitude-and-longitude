855ce38679111cd0fe0ed8510d3b8665cdf135da
juanma42 <juanmanuel@gmail.com>
2023-02-03 11:20:56 +0100

    done again cleanup

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ function UTMtoLatLon(northing, easting, zone, zoneLetter = null) {
     const mu = M / (a * (1 - e ** 2 / 4 - 3 * e ** 4 / 64 - 5 * e ** 6 / 256));
     const e1 = (1 - Math.sqrt(1 - e ** 2)) / (1 + Math.sqrt(1 - e ** 2));
     const phi1 = mu + (3 * e1 / 2 - 27 * e1 ** 3 / 32) * Math.sin(2 * mu) + (21 * e1 ** 2 / 16 - 55 * e1 ** 4 / 32) * Math.sin(4 * mu) + (151 * e1 ** 3 / 96) * Math.sin(6 * mu) + (1097 * e1 ** 4 / 512) * Math.sin(8 * mu);
-    const C = e_p ** 2 * Math.cos(phi1) ** 2 / (1 - e ** 2);
-    const T = Math.tan(phi1) ** 2;
+    /* const C = e_p ** 2 * Math.cos(phi1) ** 2 / (1 - e ** 2);
+    const T = Math.tan(phi1) ** 2; */
     const N = a / Math.sqrt(1 - e ** 2 * Math.sin(phi1) ** 2);
-    const R = a * (1 - e ** 2) / (1 - e ** 2 * Math.sin(phi1) ** 2) ** 1.5;
+    /* const R = a * (1 - e ** 2) / (1 - e ** 2 * Math.sin(phi1) ** 2) ** 1.5; */
     const phi = phi1 * 180 / Math.PI;
     const lambdas = [-177, -171, -165, -159, -153, -147, -141, -135, -129, -123, -117, -111, -105, -99, -93, -87, -81, -75, -69, -63, -57, -51, -45, -39, -33, -27, -21, -15, -9, -3, 3, 9, 15];
     const lambda0 = lambdas[zone - 1];
@@ -35,6 +35,6 @@ function UTMtoLatLon(northing, easting, zone, zoneLetter = null) {
     if (zoneLetter >= 'M' && zoneLetter <= 'Z') {
         lat = -90 + lat;
     }
-    playSound()
+    /* playSound() */
     return { lat: lat, lon: lambda };
 }
